4fbc87195120c46ad75e1c7958372275d6a4d002
Aryan bhagat <129328791+bhagataryan@users.noreply.github.com>
2024-12-31 13:26:43 +0530

    Add files via upload

---
+++ history.py
@@ -0,0 +1,418 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Tue Dec 31 11:13:03 2024)---
+import pandas as pd
+titanic = pd.read_csv(r"C:\Users\DRYDEN\Downloads\24th,26th - ML_ data preprocessing pipeline\24th- ML\TITANIC PROJECT\DATASET\titanic dataset.csv")
+titanic.tail()
+titanic.describe()
+del titanic["Name"]
+titanic.head()
+%runcell -i 0 C:/Users/DRYDEN/.spyder-py3/temp.py
+del titanic["Ticket"]
+titanic.head()
+del titanic["Fare"]
+titanic.head()
+del titanic['Cabin']
+titanic.head()
+
+def getNumber(str):
+    if str=="male":
+        return 1
+    else:
+        return 2
+titanic["Gender"]=titanic["Sex"].apply(getNumber)
+#We have created a new column called "Gender" and 
+#filling it with values 1,2 based on the values of sex column
+titanic.head()
+del titanic["Sex"]
+titanic.head()
+%runcell -i 0 C:/Users/DRYDEN/.spyder-py3/temp.py
+meanS= titanic[titanic.Survived==1].Age.mean()
+meanS
+titanic["age"]=np.where(pd.isnull(titanic.Age) & titanic["Survived"]==1  ,meanS, titanic["Age"])
+titanic.head()
+titanic.isnull().sum()
+
+meanNS=titanic[titanic.Survived==0].Age.mean()
+meanNS
+titanic.age.fillna(meanNS,inplace=True)
+titanic.head()
+titanic.isnull().sum()
+del titanic['Age']
+titanic.head()
+survivedQ = titanic[titanic.Embarked == 'Q'][titanic.Survived == 1].shape[0]
+survivedC = titanic[titanic.Embarked == 'C'][titanic.Survived == 1].shape[0]
+survivedS = titanic[titanic.Embarked == 'S'][titanic.Survived == 1].shape[0]
+print(survivedQ)
+print(survivedC)
+print(survivedS)
+survivedQ = titanic[titanic.Embarked == 'Q'][titanic.Survived == 0].shape[0]
+survivedC = titanic[titanic.Embarked == 'C'][titanic.Survived == 0].shape[0]
+survivedS = titanic[titanic.Embarked == 'S'][titanic.Survived == 0].shape[0]
+print(survivedQ)
+print(survivedC)
+print(survivedS)
+titanic.dropna(inplace=True)
+titanic.head()
+%runcell -i 0 C:/Users/DRYDEN/.spyder-py3/temp.py
+def getEmb(str):
+    if str=="S":
+        return 1
+    elif str=='Q':
+        return 2
+    else:
+        return 3
+titanic["Embark"]=titanic["Embarked"].apply(getEmb)
+titanic.head()
+%%debug
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import numpy as np
+
+titanic = pd.read_csv(r"C:\Users\DRYDEN\Downloads\24th,26th - ML_ data preprocessing pipeline\24th- ML\TITANIC PROJECT\DATASET\titanic dataset.csv")
+titanic.tail()
+
+titanic.describe()
+
+del titanic["Name"]
+titanic.head()
+
+del titanic["Ticket"]
+titanic.head()
+
+del titanic["Fare"]
+titanic.head()
+
+del titanic['Cabin']
+titanic.head()
+
+# Changing Value for "Male, Female" string values to numeric values , male=1 and female=2
+def getNumber(str):
+    if str=="male":
+        return 1
+    else:
+        return 2
+
+
+titanic["Gender"]=titanic["Sex"].apply(getNumber)
+#We have created a new column called "Gender" and 
+#filling it with values 1,2 based on the values of sex column
+titanic.head()
+
+#Deleting Sex column, since no use of it now
+del titanic["Sex"]
+titanic.head()
+
+titanic.isnull().sum()
+
+meanS= titanic[titanic.Survived==1].Age.mean()
+meanS
+
+titanic["age"]=np.where(pd.isnull(titanic.Age) & titanic["Survived"]==1  ,meanS, titanic["Age"])
+titanic.head()
+
+titanic.isnull().sum()
+
+# Finding the mean age of "Not Survived" people
+meanNS=titanic[titanic.Survived==0].Age.mean()
+meanNS
+
+titanic.age.fillna(meanNS,inplace=True)
+titanic.head()
+
+titanic.isnull().sum()
+
+del titanic['Age']
+titanic.head()
+
+
+# Finding the number of people who have survived 
+# given that they have embarked or boarded from a particular port
+
+survivedQ = titanic[titanic.Embarked == 'Q'][titanic.Survived == 1].shape[0]
+survivedC = titanic[titanic.Embarked == 'C'][titanic.Survived == 1].shape[0]
+survivedS = titanic[titanic.Embarked == 'S'][titanic.Survived == 1].shape[0]
+print(survivedQ)
+print(survivedC)
+print(survivedS)
+
+
+survivedQ = titanic[titanic.Embarked == 'Q'][titanic.Survived == 0].shape[0]
+survivedC = titanic[titanic.Embarked == 'C'][titanic.Survived == 0].shape[0]
+survivedS = titanic[titanic.Embarked == 'S'][titanic.Survived == 0].shape[0]
+print(survivedQ)
+print(survivedC)
+print(survivedS)
+
+titanic.dropna(inplace=True)
+titanic.head()
+
+titanic.isnull().sum()
+
+#Renaming "age" and "gender" columns
+titanic.rename(columns={'age':'Age'}, inplace=True)
+titanic.head()
+
+titanic.rename(columns={'Gender':'Sex'}, inplace=True)
+titanic.head()
+
+def getEmb(str):
+    if str=="S":
+        return 1
+    elif str=='Q':
+        return 2
+    else:
+        return 3
+
+
+titanic["Embark"]=titanic["Embarked"].apply(getEmb)
+titanic.head()
+
+
+#Drawing a pie chart for number of males and females aboard
+import matplotlib.pyplot as plt
+from matplotlib import style
+males = (titanic['Sex'] == 1).sum() 
+#Summing up all the values of column gender with a 
+#condition for male and similary for females
+females = (titanic['Sex'] == 2).sum()
+print(males)
+print(females)
+p = [males, females]
+plt.pie(p,    #giving array
+       labels = ['Male', 'Female'], #Correspndingly giving labels
+       colors = ['green', 'yellow'],   # Corresponding colors
+       explode = (0.15, 0),    #How much the gap should me there between the pies
+       startangle = 0)  #what start angle should be given
+plt.axis('equal') 
+plt.show()
+
+
+
+import pandas as pd
+import numpy as np
+
+titanic = pd.read_csv(r"C:\Users\DRYDEN\Downloads\24th,26th - ML_ data preprocessing pipeline\24th- ML\TITANIC PROJECT\DATASET\titanic dataset.csv")
+titanic.tail()
+
+titanic.describe()
+
+del titanic["Name"]
+titanic.head()
+
+del titanic["Ticket"]
+titanic.head()
+
+del titanic["Fare"]
+titanic.head()
+
+del titanic['Cabin']
+titanic.head()
+
+# Changing Value for "Male, Female" string values to numeric values , male=1 and female=2
+def getNumber(str):
+    if str=="male":
+        return 1
+    else:
+        return 2
+
+
+titanic["Gender"]=titanic["Sex"].apply(getNumber)
+#We have created a new column called "Gender" and 
+#filling it with values 1,2 based on the values of sex column
+titanic.head()
+
+#Deleting Sex column, since no use of it now
+del titanic["Sex"]
+titanic.head()
+
+titanic.isnull().sum()
+
+meanS= titanic[titanic.Survived==1].Age.mean()
+meanS
+
+titanic["age"]=np.where(pd.isnull(titanic.Age) & titanic["Survived"]==1  ,meanS, titanic["Age"])
+titanic.head()
+
+titanic.isnull().sum()
+
+# Finding the mean age of "Not Survived" people
+meanNS=titanic[titanic.Survived==0].Age.mean()
+meanNS
+
+titanic.age.fillna(meanNS,inplace=True)
+titanic.head()
+
+titanic.isnull().sum()
+
+del titanic['Age']
+titanic.head()
+
+
+# Finding the number of people who have survived 
+# given that they have embarked or boarded from a particular port
+
+survivedQ = titanic[titanic.Embarked == 'Q'][titanic.Survived == 1].shape[0]
+survivedC = titanic[titanic.Embarked == 'C'][titanic.Survived == 1].shape[0]
+survivedS = titanic[titanic.Embarked == 'S'][titanic.Survived == 1].shape[0]
+print(survivedQ)
+print(survivedC)
+print(survivedS)
+
+
+survivedQ = titanic[titanic.Embarked == 'Q'][titanic.Survived == 0].shape[0]
+survivedC = titanic[titanic.Embarked == 'C'][titanic.Survived == 0].shape[0]
+survivedS = titanic[titanic.Embarked == 'S'][titanic.Survived == 0].shape[0]
+print(survivedQ)
+print(survivedC)
+print(survivedS)
+
+titanic.dropna(inplace=True)
+titanic.head()
+
+titanic.isnull().sum()
+
+#Renaming "age" and "gender" columns
+titanic.rename(columns={'age':'Age'}, inplace=True)
+titanic.head()
+
+titanic.rename(columns={'Gender':'Sex'}, inplace=True)
+titanic.head()
+
+def getEmb(str):
+    if str=="S":
+        return 1
+    elif str=='Q':
+        return 2
+    else:
+        return 3
+
+
+titanic["Embark"]=titanic["Embarked"].apply(getEmb)
+titanic.head()
+
+
+#Drawing a pie chart for number of males and females aboard
+import matplotlib.pyplot as plt
+from matplotlib import style
+males = (titanic['Sex'] == 1).sum() 
+#Summing up all the values of column gender with a 
+#condition for male and similary for females
+females = (titanic['Sex'] == 2).sum()
+print(males)
+print(females)
+p = [males, females]
+plt.pie(p,    #giving array
+       labels = ['Male', 'Female'], #Correspndingly giving labels
+       colors = ['green', 'yellow'],   # Corresponding colors
+       explode = (0.15, 0),    #How much the gap should me there between the pies
+       startangle = 0)  #what start angle should be given
+plt.axis('equal') 
+plt.show()
+
+
+
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+males = (titanic['Sex'] == 1).sum() 
+#Summing up all the values of column gender with a 
+#condition for male and similary for females
+females = (titanic['Sex'] == 2).sum()
+print(males)
+print(females)
+p = [males, females]
+plt.pie(p,    #giving array
+       labels = ['Male', 'Female'], #Correspndingly giving labels
+       colors = ['green', 'yellow'],   # Corresponding colors
+       explode = (0.30, 0),    #How much the gap should me there between the pies
+       startangle = 0)  #what start angle should be given
+plt.axis('equal') 
+plt.show()
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+males = (titanic['Sex'] == 1).sum() 
+#Summing up all the values of column gender with a 
+#condition for male and similary for females
+females = (titanic['Sex'] == 2).sum()
+print(males)
+print(females)
+p = [males, females]
+plt.pie(p,    #giving array
+       labels = ['Male', 'Female'], #Correspndingly giving labels
+       colors = ['green', 'yellow'],   # Corresponding colors
+       explode = (0.30, 1),    #How much the gap should me there between the pies
+       startangle = 0)  #what start angle should be given
+plt.axis('equal') 
+plt.show()
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+males = (titanic['Sex'] == 1).sum() 
+#Summing up all the values of column gender with a 
+#condition for male and similary for females
+females = (titanic['Sex'] == 2).sum()
+print(males)
+print(females)
+p = [males, females]
+plt.pie(p,    #giving array
+       labels = ['Male', 'Female'], #Correspndingly giving labels
+       colors = ['green', 'yellow'],   # Corresponding colors
+       explode = (0.10, 0),    #How much the gap should me there between the pies
+       startangle = 0)  #what start angle should be given
+plt.axis('equal') 
+plt.show()
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+males = (titanic['Sex'] == 1).sum() 
+#Summing up all the values of column gender with a 
+#condition for male and similary for females
+females = (titanic['Sex'] == 2).sum()
+print(males)
+print(females)
+p = [males, females]
+plt.pie(p,    #giving array
+       labels = ['Male', 'Female'], #Correspndingly giving labels
+       colors = ['green', 'yellow'],   # Corresponding colors
+       explode = (0.10, 0),    #How much the gap should me there between the pies
+       startangle = 45)  #what start angle should be given
+plt.axis('equal') 
+plt.show()
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+males = (titanic['Sex'] == 1).sum() 
+#Summing up all the values of column gender with a 
+#condition for male and similary for females
+females = (titanic['Sex'] == 2).sum()
+print(males)
+print(females)
+p = [males, females]
+plt.pie(p,    #giving array
+       labels = ['Male', 'Female'], #Correspndingly giving labels
+       colors = ['green', 'yellow'],   # Corresponding colors
+       explode = (0.10, 0),    #How much the gap should me there between the pies
+       startangle = 0)  #what start angle should be given
+plt.axis('equal') 
+plt.show()
+
+MaleS=titanic[titanic.Sex==1][titanic.Survived==1].shape[0]
+print(MaleS)
+MaleN=titanic[titanic.Sex==1][titanic.Survived==0].shape[0]
+print(MaleN)
+FemaleS=titanic[titanic.Sex==2][titanic.Survived==1].shape[0]
+print(FemaleS)
+FemaleN=titanic[titanic.Sex==2][titanic.Survived==0].shape[0]
+print(FemaleN)
+chart=[MaleS,MaleN,FemaleS,FemaleN]
+colors=['lightskyblue','yellowgreen','Yellow','Orange']
+labels=["Survived Male","Not Survived Male","Survived Female","Not Survived Female"]
+explode=[0,0.05,0,0.1]
+plt.pie(chart,labels=labels,colors=colors,explode=explode,startangle=100,counterclock=False,autopct="%.2f%%")
+plt.axis("equal")
+plt.show()
